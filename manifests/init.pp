74b25d905e25e4791d95633915081da4e7ff3d38
Paul Redmond <paulrredmond@gmail.com>
2013-02-21 09:09:23 -0700

    Add the .idea folder to the global gitignore

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,19 @@
 #   include rubymine
 class rubymine {
 
+  require git::config
+
   package { 'RubyMine':
     source   => 'http://download.jetbrains.com/ruby/RubyMine-5.0.1.dmg',
     provider => 'appdmg'
+  }
+
+  $gitignore = "${git::config::configdir}/gitignore"
+
+  exec { "echo .idea >> ${gitignore}":
+    require      => File["${gitignore}"],
+    subscribe    => File["${gitignore}"],
+    refreshonly  => true,
+    onlyif       => "grep -c \.idea ${gitignore}",
   }
 }
